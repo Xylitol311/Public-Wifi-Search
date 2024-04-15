051bdd506d2e47053b7fcf1458d608f1d7cd7ec0
Xylitol311 <soohong0311@gmail.com>
2024-04-15 22:19:18 +0900

    refact: jsp 수정 및 API 키 git 추적 중지

--- src/main/java/com/example/publicwifisearch/config/api/ApiConst.java
+++
@@ -1,8 +0,0 @@
-package com.example.publicwifisearch.config.api;
-
-public class ApiConst {
-    public static final String SERVER_URL = "http://openapi.seoul.go.kr:8088";
-    public static final String AUTHENTICATION_KEY = "4d4c477171636b74313032724f6a4441";
-    public static final String SERVICE_NAME = "TbPublicWifiInfo";
-
-}
